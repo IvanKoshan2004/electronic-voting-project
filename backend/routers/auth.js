bb88916c234c841f1fbac986f764f1832fb814f3
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-03-06 19:33:31 +0200

    [15] add prisma and add small usage showcase

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,9 +1,31 @@
 import { Router } from "express";
+import { prisma } from "../lib/db.js";
 
 const authRouter = Router();
 
 authRouter.get("", (req, res) => {
-    res.json({ message: "Get request on /auth" });
+  res.json({ message: "Get request on /auth" });
+});
+
+authRouter.post("", async (req, res) => {
+  try {
+    const { username, password } = req.query;
+    const user = await prisma.user.create({
+      data: {
+        password,
+        username,
+        profile: {
+          create: {
+            phoneNumber: "10000000",
+          },
+        },
+      },
+    });
+    return res.json({ data: user });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send({ error: e });
+  }
 });
 
 export default authRouter;
