a5716d41d2d284f3201ef6ea9c28ec70fb352bbd
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-03-27 18:53:13 +0200

    [17] bring back strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import RegistrationPage from "./pages/RegistrationPage";
@@ -41,4 +42,8 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
