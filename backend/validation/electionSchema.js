035acfa6121153da6a970c973df676256fd69474
OhWellNevermind <aleksandrov.oleksii.wgm@gmail.com>
2024-04-04 09:10:48 +0300

    [24] fix error message fot voting time

--- backend/validation/electionSchema.js
+++ backend/validation/electionSchema.js
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const electionSchema = z.object({
   name: z.string({ required_error: "Election name is required" }).min(1),
   description: z.string("Desciption must be string").optional(),
-  votingTime: z.number("Time is required and must be a number"),
+  votingTime: z.number("Voting time is required and must be a number"),
   candidateNames: z.array(z.string("Candidate name is required and has to be string")),
 });
