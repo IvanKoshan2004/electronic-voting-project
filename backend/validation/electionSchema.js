a30b3c07ca3696652f9767d6422f0b5684a03977
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-04-04 11:51:29 +0300

    [24] update validation schema

--- backend/validation/electionSchema.js
+++ backend/validation/electionSchema.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const electionSchema = z.object({
   name: z.string({ required_error: "Election name is required" }).min(1),
-  description: z.string("Desciption must be string").optional(),
-  votingTime: z.number("Voting time is required and must be a number"),
-  candidateNames: z.array(z.string("Candidate name is required and has to be string")),
+  description: z.string("Description must be string").optional(),
+  votingTime: z.number("Voting time is required and must be a number").nonnegative(),
+  candidateNames: z.array(z.string("Candidate name is required and has to be string")).min(2).max(10),
 });
