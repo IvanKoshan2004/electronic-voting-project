621641f67526edd2ee67af6abc16b73a8525959d
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-04-12 10:37:24 +0300

    [28] some change

--- backend/contracts/ElectionFactory.sol
+++ backend/contracts/ElectionFactory.sol
@@ -19,9 +19,15 @@ contract ElectionFactory {
         Candidate[] candidates;
     }
 
-    mapping(uint256 => Ballot) public ballots;
-    uint256 public nextBallotId;
+    struct BallotVotes {
+        string[] voters;
+        mapping(uint8 => uint256) voteCounts;
+    }
 
+    mapping(uint256 => Ballot) public ballots;
+    mapping(uint256 => BallotVotes) internal ballotsVotes;
+
+    uint256 public nextBallotId;
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
@@ -34,7 +40,7 @@ contract ElectionFactory {
 
     event BallotCreated(uint256 _id, uint256 _createTime, uint256 _endTime);
 
-    function createBallot(string memory _name, string memory _description, uint256 _votingTime, string[] memory _candidateNames) public onlyOwner returns (Ballot memory) {
+    function createBallot(string memory _name, string memory _description, uint256 _votingTime, string[] memory _candidateNames) public onlyOwner {
 
         // check for a number of elements in given array (min. 2 candidates, max. - 10)
         require(_candidateNames.length >= 2 && _candidateNames.length <= 10, "Invalid number of candidates");
@@ -47,13 +53,51 @@ contract ElectionFactory {
         newBallot.createTime = block.timestamp;
         newBallot.endTime = newBallot.createTime + _votingTime;
 
-        for(uint i = 0; i < _candidateNames.length; i++) { // takes array of strings (candidate names) and converts it to array of structs (id + name)
+        for (uint i = 0; i < _candidateNames.length; i++) { // takes array of strings (candidate names) and converts it to array of structs (id + name)
             newBallot.candidates.push(Candidate(uint8(i), _candidateNames[i]));
         }
 
         emit BallotCreated(newBallot.id, newBallot.createTime, newBallot.endTime);
     }
 
+    event VoteSuccess();
+    event VoteFail();
+
+    function voteForCandidate(uint256 ballotId, uint8 candidateId, string memory voterId) public onlyOwner() {
+        require(ballots[ballotId].endTime < block.timestamp, "Can't vote after election has ended");
+
+        bool hasVoted = false;
+        for (uint256 i = 0; i < ballotsVotes[ballotId].voters.length; i++) {
+            if (keccak256(bytes(ballotsVotes[ballotId].voters[i])) == keccak256(bytes(candidateId))) {
+                hasVoted = true;
+                break;
+            }
+        }
+
+        // If the candidate hasn't voted yet, record the vote
+        if (!hasVoted) {
+            ballotsVotes[ballotId].voteCounts[candidateId]++;
+            ballotsVotes[ballotId].voters.push(voterId);
+            emit VoteSuccess();
+        }
+        else {
+            emit VoteFail();
+        }
+    }
+    struct CandidateVotes {
+        uint8 candidateId;
+        uint256 votesCount;
+    }
+
+    function getBallotVotes(uint256 ballotId) public view onlyOwner() returns (CandidateVotes[] memory) {
+        uint256 candidateCount = ballots[ballotId].candidates.length;
+        CandidateVotes[] memory allCandidateVotes = new CandidateVotes[](candidateCount);
+        for (uint256 i = 0; i < candidateCount; i++) {
+            Candidate memory candidate = ballots[ballotId].candidates[i];
+            allCandidateVotes[i] = CandidateVotes(candidate.id, ballotsVotes[ballotId].voteCounts[candidate.id]);
+        }
+        return allCandidateVotes;
+    }
 
     // Demonstration function
     function getAllBallots() public view returns (Ballot[] memory) {
