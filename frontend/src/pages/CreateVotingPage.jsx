ec00b98aa03496e4f68357f08ff7011d5e26c272
IhorKoss <kos.ig.iv@gmail.com>
2024-04-21 20:27:09 +0300

    fixed everything

--- frontend/src/pages/CreateVotingPage.jsx
+++ frontend/src/pages/CreateVotingPage.jsx
@@ -1,12 +1,50 @@
 import css from "./CreateVotingPage.module.css";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export function CreateVotingPage() {
+  const [fields, setFields] = useState([
+    { id: 1, value: "" },
+    { id: 2, value: "" },
+  ]);
+  const [convertedTime, setConvertedTime] = useState("");
+  const [clearCandidates, setClearCandidates] = useState([]);
+  const {
+    handleSubmit,
+    register,
+    formState: { isValid, errors },
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = function (data) {
+    const splitedTime = data.time.split(":").map(Number);
+    setConvertedTime(splitedTime[0] * 86400 + splitedTime[1] * 3600 + splitedTime[2] * 60 + splitedTime[3]);
+    setClearCandidates(data.candidates.filter(candidate => candidate));
+  };
+  const handleChange = function (id, value) {
+    const newFields = [...fields];
+    const index = newFields.findIndex(field => field.id === id);
+    newFields[index].value = value;
+    if (index === fields.length - 1 && index < 9 && value !== "") {
+      newFields.push({ id: fields.length + 1, value: "" });
+    }
+    setFields(newFields);
+  };
   return (
-    <form className={css.formBlock}>
+    <form className={css.formBlock} onSubmit={handleSubmit(onSubmit)}>
       <h1 className={css.creatingHeader}>create voting</h1>
       <div className={css.inputBlock}>
         <p className={css.inputDescription}>name:</p>
-        <input placeholder="enter name here..." type="text" />
+        <input
+          placeholder="enter name here..."
+          type="text"
+          {...register("name", {
+            required: true,
+            minLength: { value: 3, message: "Minimum 3 characters" },
+          })}
+        />
+        <p className={css.errorMessage}>{errors.name?.message}</p>
       </div>
       <div className={css.inputBlock}>
         <p className={css.inputDescription}>description:</p>
@@ -14,30 +52,39 @@ export function CreateVotingPage() {
       </div>
       <div className={css.inputBlock}>
         <p className={css.inputDescription}>voting end time:</p>
-        <input placeholder="dd:hh:mm:ss" type="text" />
+        <input
+          placeholder="dd:hh:mm:ss"
+          type="text"
+          {...register("time", {
+            required: true,
+            pattern: {
+              value: /^(\d\d):([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/,
+              message: "Please, follow this format: dd:hh:mm:ss",
+            },
+          })}
+        />
+        <div className={css.errorMessage}>{errors.time?.message}</div>
       </div>
       <div className={css.candidateBlock}>
         <div className={css.candidateNumberBlock}>
-          <p className={css.inputDescription}>enter number of candidates (2-10):</p>
-          <div className={css.candidateCount}>2</div>
+          <p className={css.inputDescription}>number of candidates (2-10):</p>
+          <div className={css.candidateCount}>{fields.length}</div>
         </div>
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
-        <input placeholder="enter your candidate here..." type="text" />
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <input
+              type="text"
+              {...register(`candidates.${index}`, {
+                required: fields.length < 3 ? true : false,
+              })}
+              value={field.value}
+              onChange={e => handleChange(field.id, e.target.value)}
+              placeholder="enter your candidate here..."
+            />
+          </div>
+        ))}
       </div>
-      <button type="submit" className={css.createBtn}>
+      <button type="submit" className={css.createBtn} disabled={!isValid}>
         Create
       </button>
     </form>
